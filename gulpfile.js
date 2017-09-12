f8bdfb4c66fc8554c3fdfba548af7cc3a9546d2c
morgan Peck <mlpeck@fullsail.edu>
2017-09-11 21:36:13 -0500

    starting to build my porfolio adding some sections for each part of my home page and also messing around with my bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp=require('gulp')
+var sass=require('gulp-sass')
+var browserSync = require('browser-sync').create()
+gulp.task('default',function(){
+
+})
+gulp.task('scss',function(){
+return gulp.src('./scss/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./css'))
+  .pipe(browserSync.reload({
+    stream:true
+  }))
+})
+gulp.task('browser-sync',function(){
+  browserSync.init({
+    browser: 'google chrome',
+  server: {
+    baseDir:"./"
+    
+  }
+})})
+gulp.task('watch',['browser-sync','scss'],function(){
+  gulp.watch('./scss/**/*.scss',['scss'])
+})
