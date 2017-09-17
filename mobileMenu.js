75a6fd4590ad1b0de530c80035232e6e050cf12a
morgan Peck <mlpeck@fullsail.edu>
2017-09-17 00:09:53 -0500

    adding my mobile navigation overlay

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,5 +1,14 @@
-var hamburger=document.querySelector(".hamburgerMenu");
-function openMenu
+var hamburger=document.querySelector(".overlay");
+
+function openMenu()
 {
-    if{}
+    if(hamburger.className.match('overlay')){
+        hamburger.className='opened';
+        var menuOpen=document.querySelector(".opened").style.width="100%";
+    }
+    else{
+        hamburger.className='overlay';
+    }
+    
 }
+hamburger.addEventListener('click',openMenu, false);
